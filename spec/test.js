a7122634c705ca9c9d3abe154a856367306d388d
Jithin Nair <jitnair@deloitte.com>
2018-10-02 13:38:11 +0530

    topic 1

---
+++ spec/test.js
@@ -0,0 +1,14 @@
+/**
+ * This test is picked up by mocha to run the tests.
+ * So the file or the module you imported will be executed
+ *
+ * Eg:
+ * "../modules/arrow/exercise" - exercise
+ * "../modules/arrow/solution" - executing the solution
+ */
+
+// TO RUN individual module
+import "../modules/FP/solution";
+
+// TO RUN ALL TESTS
+// import "../modules";
